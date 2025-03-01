bc1d062b60d1c809829f47abff6e03fe3d7f83b5
pk00749 <pk00749@163.com>
2025-03-01 22:24:35 +0800

    init

--- exercise_llama.py
+++ exercise_llama.py
@@ -1,4 +1,3 @@
-import json
 import warnings
 import time
 from llama_cpp import Llama
@@ -10,6 +9,16 @@ warnings.filterwarnings("ignore")
 
 model_path = 'pretrained_models/DeepSeek-R1-Distill-Qwen-14B-GGUF'
 gguf_file_name = 'DeepSeek-R1-Distill-Qwen-14B-Q8_0.gguf'
+
+template = """
+根据以下上下文来回答问题。如果不知道答案，就直接说不知道，不能试图编造答案。
+上下文: {context}
+
+问题: {question}
+
+答案: 
+"""
+
 
 def init_existing_vector_database(question):
     embedding_function = HuggingFaceBgeEmbeddings(model_name="pretrained_models/bge-large-zh")
@@ -23,16 +32,10 @@ def init_pretrained_model():
     llm = Llama(model_path=f"./{model_path}/{gguf_file_name}")
     return llm
 
-def create_prompt_template():
-    template = """引用: {context}
-    问题: {question}
-    答案: 我一步一步推理..."""
-    prompt = PromptTemplate.from_template(template)
-    return prompt
-
 def create_chat(llm, question, context):
-    prompt = create_prompt_template()
+    prompt = PromptTemplate.from_template(template)
     formatted_prompt = prompt.format(question=question, context=context)
+    print(formatted_prompt)
     output = llm(prompt=formatted_prompt, max_tokens=1500)
     print(output['choices'][0]['text'])
 
@@ -48,4 +51,4 @@ def chat(question):
 
 # 示例运行
 if __name__ == "__main__":
-    chat("刘备的儿子是谁")
+    chat("刘备最后统一三国")
